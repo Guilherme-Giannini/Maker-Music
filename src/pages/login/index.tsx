8b2ab34d20de1f34fa4d88baf2110e557ffbcdf3
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-08-19 10:33:40 -0300

    Criando Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,11 +3,18 @@ import {
     Text,
     View,
     Image,
-    TextInput
+    TextInput,
+    TouchableOpacity,
 } from 'react-native';
 import { style } from './style';
 import Logo from '../../assets/logo.png';
+
 export default function Login() {
+
+    const handleLogin = () => {
+        console.log("Entrar clicado!");
+    };
+
     return (
         <View style={style.container}>  
             <View style={style.boxtop}>
@@ -17,18 +24,34 @@ export default function Login() {
                 />
                 <Text style={style.text}>Bem Vindo!</Text>
             </View>
+
+          
             <View style={style.boxmid}>
-                <Text>Email:</Text>
+                <Text style={style.text}>Email:</Text>
                 <TextInput 
-                    placeholder='Digite seu email'>    
-                    </TextInput>
-                    <Text>Senha:</Text>
+                    style={style.emailInput}
+                    placeholder='Digite seu email'
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+
+                <Text style={style.text}>Senha:</Text>
                 <TextInput 
-                    placeholder='Digite sua senha'>    
-                    </TextInput>
+                    style={style.senhaInput}
+                    placeholder='Digite sua senha'
+                    secureTextEntry={true}
+                />
+
+            
+                <TouchableOpacity style={style.button} onPress={handleLogin}>
+                    <Text style={style.buttonText}>Entrar</Text>
+                </TouchableOpacity>
             </View>
-            <View style={style.boxbottom}>  
-                <Text>boxbottom</Text>
+
+            <View style={style.boxbottom}>
+             <TouchableOpacity style={style.forget} onPress={handleLogin}>
+                    <Text style={style.buttonText}>Esqueci a senha</Text>
+                </TouchableOpacity>
             </View>
         </View>  
     );
